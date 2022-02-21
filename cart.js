21f8a5e520432ff92e4a57c4f789592e7744bba3
Arista <89002597+arista-hsieh@users.noreply.github.com>
2022-02-21 16:21:28 +0800

    Add files via upload

--- cart.js
+++ cart.js
@@ -1,13 +1,43 @@
 const apiUrl = 'https://vue3-course-api.hexschool.io/v2';
 const apiPath = 'arista';
 
+const { defineRule, Form, Field, ErrorMessage, configure } = VeeValidate;
+const { required, email, min, max } = VeeValidateRules;
+const { localize, loadLocaleFromURL } = VeeValidateI18n;
+
+defineRule('required', required);
+defineRule('email', email);
+defineRule('min', min);
+defineRule('max', max);
+
+loadLocaleFromURL('https://unpkg.com/@vee-validate/i18n@4.1.0/dist/locale/zh_TW.json');
+
+configure({
+  generateMessage: localize('zh_TW'),
+});
+
 Vue.createApp({
     data(){
         return{
             cartData:{},
             products:[],
             productId:'',
+            isLoadingItem:'',
+            form: {
+                user: {
+                  name: '',
+                  email: '',
+                  tel: '',
+                  address: '',
+                },
+                message: '',
+              },
         };
+    },
+    components: {
+        VForm: Form,
+        VField: Field,
+        ErrorMessage: ErrorMessage,
     },
     methods:{
         getProducts(){
@@ -19,10 +49,61 @@ Vue.createApp({
         openProductModal(id){
             this.productId = id;
             this.$refs.productModal.openModal();
-        }
+        },
+        getCart(){
+            axios.get(`${apiUrl}/api/${apiPath}/cart`)
+            .then(res =>{
+                this.cartData = res.data.data;
+            });
+        },
+        updateCartItem(item){
+            const data = {
+                product_id: item.id,
+                qty: item.qty,
+            };
+            this.isLoadingItem = item.id;
+            axios.put(`${apiUrl}/api/${apiPath}/cart/${item.id}`,{ data })
+            .then(res =>{
+                
+                this.getCart();
+                this.isLoadingItem = '';
+            });
+        },
+        removeCartItem(id){
+            this.isLoadingItem = id;
+            axios.delete(`${apiUrl}/api/${apiPath}/cart/${id}`)
+            .then(res =>{
+                this.getCart();
+                this.isLoadingItem = '';
+            });
+        },
+        addToCart(id,qty = 1){
+            const data = {
+                product_id: id,
+                qty,
+            };
+            this.isLoadingItem = id;
+            axios.post(`${apiUrl}/api/${apiPath}/cart`,{ data })
+            .then(res =>{
+                
+                this.getCart();
+                this.$refs.productModal.closeModal();
+                this.isLoadingItem = '';
+            });
+        },
+        deleteAllCarts() {
+            const url = `${apiUrl}/api/${apiPath}/carts`;
+            axios.delete(url).then((response) => {
+              alert(response.data.message);
+              this.getCart();
+            }).catch((err) => {
+              alert(err.data.message);
+            });
+        },
     },
     mounted(){
         this.getProducts();
+        this.getCart();
     }
 })
 .component('product-modal',{
@@ -32,6 +113,7 @@ Vue.createApp({
         return{
             modal:{},
             product:{},
+            qty:1,
         };
     },
     watch:{
@@ -43,11 +125,17 @@ Vue.createApp({
         openModal(){
             this.modal.show();
         },
+        closeModal(){
+            this.modal.hide();
+        },
         getProduct(){
             axios.get(`${apiUrl}/api/${apiPath}/product/${this.id}`)
             .then(res =>{
                 this.product = res.data.product;
             });
+        },
+        addToCart(){
+            this.$emit('add-cart',this.product.id,this.qty);
         },
     },
     mounted(){
